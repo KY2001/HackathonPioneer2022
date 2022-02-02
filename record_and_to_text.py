647e49f42f1f68b9b206fc6ee0b36b4aaae9f7c2
Kein Yukiyoshi <62263364+KY2001@users.noreply.github.com>
2022-02-02 18:13:05 +0900

    init

---
+++ record_and_to_text.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+
+def record_and_to_text():  # 音声を録音し、文字を返す
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("=== 何か、話しかけてください ===")
+        audio = r.listen(source)
+        print("=== ここまでの音声を録音しました ===")
+        text = r.recognize_google(audio, language="ja-JP")
+        print(text)
+
+
+record_and_to_text()
