fe96bf25ebcb6cd5d90c4e5d2562dcbb4329b466
Kein Yukiyoshi <62263364+KY2001@users.noreply.github.com>
2022-02-03 11:29:26 +0900

    day2-1

---
+++ GUI.py
@@ -0,0 +1,23 @@
+import PySimpleGUI as sg
+import record_and_to_text as r2t
+
+sg.theme('DarkAmber')   # デザインテーマの設定
+
+# ウィンドウに配置するコンポーネント
+layout = [  [sg.Button('実行'), sg.Button('キャンセル')],
+            [sg.MLine(key='-ML1-'+sg.WRITE_ONLY_KEY,size=(20,10))]
+        ]
+
+# ウィンドウの生成
+window = sg.Window('サンプルプログラム', layout)
+
+# イベントループ
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'キャンセル':
+        break
+    elif event == '実行':
+        text = r2t.record_and_to_text()
+        window['-ML1-'+sg.WRITE_ONLY_KEY].print(text)
+
+window.close()
--- main.py
+++ main.py
@@ -3,46 +3,16 @@ from record_and_to_text import *
 from play_and_text_to_speech import *
 
 text = record_and_to_text()
-whday = 0
-trivia = 0
-theme = 0
-maxtr = 2
-maxwh = 2
-maxth = 2
 
-"""
-if "音楽" in text and "流" in text:
-  #音楽を流す
-elif "話題" in text and "ない" in text:
-  if
-"""
+if "音楽" in text or "流" in text:
+    auto_speech("音楽を再生します。")
+    play("music.mp3")
 
-if "今日" in text and "何" in text:
-    if whday == 0:
-        # 今日は大豆の日です
-        auto_speech("今日は大豆の日です")
-        whday += 1
-    elif whday == 1:
-        # 今日は乳酸菌の日です
-        auto_speech("今日は乳酸菌の日です")
-        whday += 1
-    elif whday == 2:
-        # 今日は英雄の日です
-        print("今日は英雄の日です")
-        whday = 0
-    if trivia < maxwh:
-        whday += 1
+if "バンド" in text or "名前" in text:
+    auto_speech("このアーティストはスピッツです。")
 
-""""
-  else : whday = 0
-elif "何か" in text and "教えて" in text:
-  if trivia == 0:
-    #シロクマはだいたい左利き
-  elif trivia == 1:
-    #カタツムリには歯がしこたまある
-  elif trivia ==2 :
-    #カビバラはウサインボルトよりはやい
-  if trivia < maxtr:
-    trivia +=1
-  else : trivia = 0
-  """
+if "昨日" in text or "ゴルフ" in text:
+    auto_speech("お父様の昨日の目的地は銀座です。")
+
+if "やましい" in text:
+    auto_speech("お母様、グローブボックスを開けてください。")
